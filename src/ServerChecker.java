faa07665e1100fbcbd1b562f2f9bce0960c57e32
seokhee98@naver.com <seokhee98@naver.com>
2014-05-16 03:16:19 +0900

    file change

--- src/ServerChecker.java
+++ src/ServerChecker.java
@@ -1,20 +1,32 @@
+import java.io.File;
+import java.io.FileOutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 
 public class ServerChecker {
 
 	private int totalCnt = 0;
+	private int checkErrorCount = 3;
+	private int checkErrorTerm = 5000;
+	private String checkUrl;
+	private final String LOG_FILE_NAME = "server_checker.log";
+	private Calendar cal = Calendar.getInstance();
+	private SimpleDateFormat sdf = new SimpleDateFormat("");
 	
 	public ServerChecker() {
 		//checkUrl();
 	}
 	
+	
+	
 	private void checkUrl() {
 		URL url = null;
 		
 		try{
-			url = new URL("http://122.199.152.200:8181/login.do");
+			url = new URL(checkUrl);
 			HttpURLConnection conn = (HttpURLConnection)url.openConnection();
 			conn.connect();
 			int result = conn.getResponseCode();
@@ -31,13 +43,13 @@ public class ServerChecker {
 		}
 	}
 	
-	private void aaa() {
+	private void startUrlCheck() {
 		while(true) {
 			try{
-				Thread.sleep(5000);
+				Thread.sleep(checkErrorTerm);
 				checkUrl();
-				
-				if(totalCnt > 3) {
+				logToFile("");
+				if(totalCnt > checkErrorCount) {
 					stop();
 					break;
 				}
@@ -48,6 +60,13 @@ public class ServerChecker {
 		}
 	}
 	
+	private void logToFile(String msg) throws Exception {
+		File f = new File("/Users/sayit/a.txt");
+		FileOutputStream fos = new FileOutputStream(f, true);
+		msg = msg + cal.toString();
+		fos.write(msg.getBytes());
+		fos.close();
+	}
 	private void stop() {
 		System.out.println("stop");
 		start();
@@ -57,7 +76,7 @@ public class ServerChecker {
 		System.out.println("start");
 		totalCnt = 0;
 		
-		aaa();
+		startUrlCheck();
 	}
 
 	
@@ -67,7 +86,59 @@ public class ServerChecker {
 	 */
 	public static void main(String[] args) {
 		ServerChecker c = new ServerChecker();
-		c.aaa();
+		c.setCheckUrl("http://122.199.152.200:8181/login.do");
+		c.startUrlCheck();
 	}
+
+
+	/**
+	 * 체크할 주소를 반환한다.
+	 * @return
+	 */
+	public String getCheckUrl() {
+		return checkUrl;
+	}
+	
+	/**
+	 * 체크할 주소를 설정한다.
+	 * @param checkUrl
+	 */
+	public void setCheckUrl(String checkUrl) {
+		this.checkUrl = checkUrl;
+	}
+	
+	/**
+	 * 체크할 에러수를 설정한다.
+	 * @param checkErrorCount
+	 */
+	public void setCheckErrorCount(int checkErrorCount) {
+		this.checkErrorCount = checkErrorCount;
+	}
+	
+	/**
+	 * 체크할 에러수를 반환한다.
+	 * @return
+	 */
+	public int getCheckErrorCount() {
+		return this.checkErrorCount;
+	}
+	
+	/**
+	 * 체크주기를 세팅한다.
+	 * @param checkErrorTerm 밀리세컨드
+	 */
+	public void setCheckErrorTerm(int checkErrorTerm) {
+		this.checkErrorTerm = checkErrorTerm;
+	}
+	
+	/**
+	 * 체크주기를 반환한다.
+	 * @return 밀리세컨드
+	 */
+	public int getCheckErrorTerm() {
+		return this.checkErrorTerm;
+	}
+	
+	
 
 }
